9f9fc9aad5bf0ffcfa8288bb791b3d69dac5ea7d
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-03-18 15:35:18 +0100

    🛹 Add choice to print with bluetooth or usb printer.

--- App.js
+++ App.js
@@ -16,19 +16,35 @@ export default function App() {
   const [currentBluetoothPrinter, setCurrentBluetoothPrinter] = useState();
   const [usbPrinters, setUsbPrinters] = useState([]);
   const [currentUsbPrinter, setCurrentUsbPrinter] = useState();
-  const [selectedLanguage, setSelectedLanguage] = useState();
+  const [selectedDeviceType, setSelectedDeviceType] = useState('Bluetooth');
 
   // on component did mount
   useEffect(() => {
+    refreshAllDevices();
+  }, []);
+
+  // refresh all type of devices
+  const refreshAllDevices = () => {
+    refreshBluetoothDevices();
+    refreshUsbDevices();
+  };
+
+  // refresh bluetooth devices
+  const refreshBluetoothDevices = () => {
     BLEPrinter.init().then(() => {
       BLEPrinter.getDeviceList().then(setBluetoothPrinters);
     });
+  };
+
+  // refresh usb devices
+  const refreshUsbDevices = () => {
+    // works only for android
     if (Platform.OS == 'android') {
       USBPrinter.init().then(() => {
         USBPrinter.getDeviceList().then(setUsbPrinters);
       });
     }
-  }, []);
+  };
 
   // connect to a usb printer
   const connectUsbPrinter = printer => {
@@ -38,6 +54,7 @@ export default function App() {
     );
     ToastAndroid.show('Connected to a usb printer', ToastAndroid.LONG);
   };
+
   // print sample text from usb printer
   const printUsbTextTest = () => {
     currentUsbPrinter && USBPrinter.printText('<CB>Hello, World!</CB>\n');
@@ -45,7 +62,49 @@ export default function App() {
 
   // print bill from usb printer
   const printUsbBillTest = () => {
-    currentUsbPrinter && USBPrinter.printText('<C>Hello, World! bill</C>');
+    currentUsbPrinter && USBPrinter.printBill('<C>Hello, World! bill</C>');
+  };
+
+  // print sample bill with custom inputs from usb printer
+  const printUsbBillSample = (
+    storeName,
+    address,
+    phoneNumber,
+    productsList,
+    collectedAmount,
+    discount,
+    charge,
+    total,
+    ticketNumber,
+    thanks,
+    numberOfCopies,
+  ) => {
+    if (numberOfCopies <= 0) {
+      console.warn('numberOfCpise must be over 1!');
+      return;
+    }
+    const line = '<C>--------------------------------------------</C>\n';
+    const date = new Date().toLocaleString('fr-fr');
+    for (let i = 0; i < numberOfCopies; i++) {
+      USBPrinter.printBill(
+        `<CB>${storeName}</CB>\n` +
+          `<CM>${address}</CM>\n` +
+          `<CM>${phoneNumber}</CM>\n` +
+          line +
+          ',' +
+          productsList.map(product => '* ' + product + ' DA\n') +
+          line +
+          `Collected Amount : ${collectedAmount} DA\n` +
+          `Discount : ${discount} DA\n` +
+          `Charge : ${charge} DA\n` +
+          `<CD>Total : ${total} DA</CD>\n` +
+          line +
+          `Ticket Number : #${ticketNumber}\n` +
+          `${date}\n` +
+          `<CM>${thanks}</CM>\n`,
+      );
+      sleep(1000);
+    }
   };
 
   // connect to a bluetooth printer
@@ -68,7 +127,7 @@ export default function App() {
       BLEPrinter.printBill('<C>Hello, World! bill</C>');
   };
 
-  // print sample bill with custom inputs
+  // print sample bill with custom inputs from bluetooth printer
   const printBluetoothBillSample = (
     storeName,
     address,
@@ -82,15 +141,11 @@ export default function App() {
     thanks,
     numberOfCopies,
   ) => {
-<<<<<<< HEAD
-    const line = '<C>-------------------------------  -----------------</C>\n';
-=======
     if (numberOfCopies <= 0) {
       console.warn('numberOfCpise must be over 1!');
       return;
     }
     const line = '<C>--------------------------------------------</C>\n';
->>>>>>> 6e5fe1b338ee12e6f47908af424f42fda330c363
     const date = new Date().toLocaleString('fr-fr');
     for (let i = 0; i < numberOfCopies; i++) {
       BLEPrinter.printBill(
@@ -142,14 +197,7 @@ export default function App() {
           <Button
             title={'refresh'}
             onPress={() => {
-              BLEPrinter.init().then(() => {
-                BLEPrinter.getDeviceList().then(setBluetoothPrinters);
-              });
-              if (Platform.OS == 'android') {
-                USBPrinter.init().then(() => {
-                  USBPrinter.getDeviceList().then(setUsbPrinters);
-                });
-              }
+              refreshAllDevices();
             }}
           />
           <View style={{flex: 1}}>
@@ -202,6 +250,29 @@ export default function App() {
             padding: 10,
             borderRadius: 20,
           }}>
+          {currentUsbPrinter && (
+            <Text style={{color: 'green', fontSize: 30}}>
+              USB printer connected
+            </Text>
+          )}
+          {currentBluetoothPrinter && (
+            <Text style={{color: 'green', fontSize: 30}}>
+              Bluetooth printer connected
+            </Text>
+          )}
+          <View
+            style={{
+              backgroundColor: 'white',
+            }}>
+            <Picker
+              selectedValue={selectedDeviceType}
+              onValueChange={(itemValue, itemIndex) =>
+                setSelectedDeviceType(itemValue)
+              }>
+              <Picker.Item label="Bluetooth" value="Bluetooth" />
+              <Picker.Item label="USB" value="USB" />
+            </Picker>
+          </View>
           <TouchableOpacity
             style={{
               backgroundColor: 'red',
@@ -211,7 +282,11 @@ export default function App() {
               alignItems: 'center',
               justifyContent: 'center',
             }}
-            onPress={printBluetoothTextTest}>
+            onPress={() => {
+              selectedDeviceType === 'Bluetooth'
+                ? printBluetoothTextTest()
+                : printUsbTextTest();
+            }}>
             <Text style={{color: 'white', fontSize: 30}}>Print Text</Text>
           </TouchableOpacity>
           <TouchableOpacity
@@ -223,7 +298,11 @@ export default function App() {
               alignItems: 'center',
               justifyContent: 'center',
             }}
-            onPress={printBluetoothBillTest}>
+            onPress={() => {
+              selectedDeviceType === 'Bluetooth'
+                ? printBluetoothBillTest()
+                : printUsbBillTest();
+            }}>
             <Text style={{color: 'white', fontSize: 30}}>Print Bill Text</Text>
           </TouchableOpacity>
           <TouchableOpacity
@@ -235,42 +314,46 @@ export default function App() {
               alignItems: 'center',
               justifyContent: 'center',
             }}
-            onPress={() =>
-              printBluetoothBillSample(
-                'Omar Store', // storeName
-                'Mesra, Mostaganem', // address
-                '04516564545', // phoneNumber
-                ['Batata : 200.0', 'Khobz : 10.0', 'Dela3 : 300.0'], // productsList
-                '5600', // collectedAmount
-                '0.0', // discount
-                '0.0', // charge
-                '5600', // total
-                '1125', // ticketNumber
-                'Merci pour votre achat', // thanks
-<<<<<<< HEAD
-                2,
-=======
-                0,
->>>>>>> 6e5fe1b338ee12e6f47908af424f42fda330c363
-              )
-            }>
+            onPress={() => {
+              switch (selectedDeviceType) {
+                case 'Bluetooth':
+                  break;
+
+                default:
+                  break;
+              }
+              selectedDeviceType === 'Bluetooth'
+                ? printBluetoothBillSample(
+                    'Omar Store', // storeName
+                    'Mesra, Mostaganem', // address
+                    '04516564545', // phoneNumber
+                    ['Batata : 200.0', 'Khobz : 10.0', 'Dela3 : 300.0'], // productsList
+                    '5600', // collectedAmount
+                    '0.0', // discount
+                    '0.0', // charge
+                    '5600', // total
+                    '1125', // ticketNumber
+                    'Merci pour votre achat', // thanks
+                    1, // numberOfCopies
+                  )
+                : printUsbBillSample(
+                    'Omar Store', // storeName
+                    'Mesra, Mostaganem', // address
+                    '04516564545', // phoneNumber
+                    ['Batata : 200.0', 'Khobz : 10.0', 'Dela3 : 300.0'], // productsList
+                    '5600', // collectedAmount
+                    '0.0', // discount
+                    '0.0', // charge
+                    '5600', // total
+                    '1125', // ticketNumber
+                    'Merci pour votre achat', // thanks
+                    1, // numberOfCopies
+                  );
+            }}>
             <Text style={{color: 'white', fontSize: 30}}>
               Print Sample Bill
             </Text>
           </TouchableOpacity>
-          <View
-            style={{
-              backgroundColor: 'white',
-            }}>
-            <Picker
-              selectedValue={selectedLanguage}
-              onValueChange={(itemValue, itemIndex) =>
-                setSelectedLanguage(itemValue)
-              }>
-              <Picker.Item label="bluetooth" value="bluetooth" />
-              <Picker.Item label="usb" value="usb" />
-            </Picker>
-          </View>
         </View>
       </View>
     </View>
--- android/app/src/main/java/com/sunmiprinter/MainApplication.java
+++ android/app/src/main/java/com/sunmiprinter/MainApplication.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.reactnativecommunity.picker.RNCPickerPackage;
+import com.reactnativecommunity.picker.RNCPickerPackage;
 import com.pinmi.react.printer.RNPrinterPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
