9335de4e6b48e3fcbdb6adf808d59abc1b6cb11f
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-03-18 09:10:18 +0100

    ☄ Get list of devices and display them.

--- App.js
+++ App.js
@@ -1,7 +1,37 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
+import {BLEPrinter} from 'react-native-thermal-receipt-printer';
 
 export default function App() {
+  // states
+  const [printers, setPrinters] = useState([]);
+  const [currentPrinter, setCurrentPrinter] = useState();
+
+  // on component dit mount
+  useEffect(() => {
+    BLEPrinter.init().then(() => {
+      BLEPrinter.getDeviceList().then(setPrinters);
+    });
+  }, []);
+
+  // connect to printer
+  const connectPrinter = printer => {
+    BLEPrinter.connectPrinter(printer.inner_mac_address).then(
+      setCurrentPrinter,
+      error => console.warn(error),
+    );
+  };
+
+  // print sample text
+  const printTextTest = () => {
+    currentPrinter && BLEPrinter.printText('<C>Hello, World! text</C>\n');
+  };
+
+  // print bill
+  const printBillTest = () => {
+    currentPrinter && BLEPrinter.printBill('<C>Hello, World! bill</C>');
+  };
+
   return (
     <View
       style={{
@@ -10,7 +40,22 @@ export default function App() {
         alignItems: 'center',
         backgroundColor: 'black',
       }}>
-      <Text style={{fontSize: 50, color: 'white'}}>Hello</Text>
+      <Text style={{color: 'white', fontSize: 50}}>List of devices</Text>
+      {printers.map(printer => (
+        <TouchableOpacity
+          style={{
+            backgroundColor: 'blue',
+            borderRadius: 30,
+            padding: 5,
+            margin: 5,
+          }}
+          key={printer.inner_mac_address}
+          onPress={() => connectPrinter(printer)}>
+          <Text style={{color: 'white', fontSize: 30}}>
+            {`device_name: ${printer.device_name}, inner_mac_address: ${printer.inner_mac_address}`}
+          </Text>
+        </TouchableOpacity>
+      ))}
     </View>
   );
 }
