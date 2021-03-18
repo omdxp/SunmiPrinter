944fc9a31351d36aadb95ebb65dffb17c3f05025
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-03-18 09:33:45 +0100

    🧵 Get usb devices and display them.

--- App.js
+++ App.js
@@ -1,71 +1,153 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
-import {BLEPrinter} from 'react-native-thermal-receipt-printer';
+import {View, Text, TouchableOpacity, Platform} from 'react-native';
+import {BLEPrinter, USBPrinter} from 'react-native-thermal-receipt-printer';
 
 export default function App() {
   // states
-  const [printers, setPrinters] = useState([]);
-  const [currentPrinter, setCurrentPrinter] = useState();
+  const [bluetoothPrinters, setBluetoothPrinters] = useState([]);
+  const [currentBluetoothPrinter, setCurrentBluetoothPrinter] = useState();
+  const [usbPrinters, setUsbPrinters] = useState([]);
+  const [currentUsbPrinter, setCurrentUsbPrinter] = useState();
 
-  // on component dit mount
+  // on component did mount
   useEffect(() => {
     BLEPrinter.init().then(() => {
-      BLEPrinter.getDeviceList().then(setPrinters);
+      BLEPrinter.getDeviceList().then(setBluetoothPrinters);
     });
+    if (Platform.OS == 'android') {
+      USBPrinter.init().then(() => {
+        USBPrinter.getDeviceList().then(setUsbPrinters);
+      });
+    }
   }, []);
 
-  // connect to printer
-  const connectPrinter = printer => {
-    BLEPrinter.connectPrinter(printer.inner_mac_address).then(
-      setCurrentPrinter,
-      error => console.warn(error),
+  // connect to a usb printer
+  const connectUsbPrinter = printer =>
+    USBPrinter.connectPrinter(printer.vendorID, printer.productId).then(() =>
+      setCurrentUsbPrinter(printer),
     );
+
+  // print sample text from usb printer
+  const printUsbTextTest = () => {
+    currentUsbPrinter && USBPrinter.printText('<C>Hello, World! text</C>\n');
   };
 
-  // print sample text
-  const printTextTest = () => {
-    currentPrinter && BLEPrinter.printText('<C>Hello, World! text</C>\n');
+  // print bill from usb printer
+  const printUsbBillTest = () => {
+    currentUsbPrinter && USBPrinter.printText('<C>Hello, World! bill</C>');
   };
 
-  // print bill
-  const printBillTest = () => {
-    currentPrinter && BLEPrinter.printBill('<C>Hello, World! bill</C>');
+  // connect to a bluetooth printer
+  const connectBluetoothPrinter = printer => {
+    BLEPrinter.connectPrinter(
+      printer.inner_mac_address,
+    ).then(setCurrentBluetoothPrinter, error => console.warn(error));
+  };
+
+  // print sample text from bluetooth printer
+  const printBluetoothTextTest = () => {
+    currentBluetoothPrinter &&
+      BLEPrinter.printText('<C>Hello, World! text</C>\n');
+  };
+
+  // print bill from bluetooth printer
+  const printBluetoothBillTest = () => {
+    currentBluetoothPrinter &&
+      BLEPrinter.printBill('<C>Hello, World! bill</C>');
   };
 
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'black',
+        padding: 10,
+        backgroundColor: '#21edff',
       }}>
-      <Text style={{color: 'white', fontSize: 50}}>List of devices</Text>
-      {printers.map(printer => (
-        <TouchableOpacity
+      <View style={{flex: 1, flexDirection: 'row'}}>
+        <View
           style={{
-            backgroundColor: 'blue',
-            borderRadius: 30,
-            padding: 5,
-            margin: 5,
-          }}
-          key={printer.inner_mac_address}
-          onPress={() => connectPrinter(printer)}>
-          <Text style={{color: 'white', fontSize: 30}}>
-            {`device_name: ${printer.device_name}, inner_mac_address: ${printer.inner_mac_address}`}
-          </Text>
-        </TouchableOpacity>
-      ))}
-      <TouchableOpacity
-        style={{
-          backgroundColor: 'red',
-          borderRadius: 30,
-          padding: 5,
-          margin: 5,
-        }}
-        onPress={printTextTest}>
-        <Text style={{color: 'white', fontSize: 30}}>Print Text</Text>
-      </TouchableOpacity>
+            flex: 2,
+            backgroundColor: 'black',
+            margin: 20,
+            padding: 10,
+            borderRadius: 20,
+          }}>
+          <View style={{flex: 1}}>
+            <Text style={{color: 'white', fontSize: 50}}>
+              List of bluetooth devices
+            </Text>
+            {bluetoothPrinters.map(printer => (
+              <TouchableOpacity
+                style={{
+                  backgroundColor: 'blue',
+                  borderRadius: 30,
+                  padding: 5,
+                  margin: 5,
+                }}
+                key={printer.inner_mac_address}
+                onPress={() => connectBluetoothPrinter(printer)}>
+                <Text style={{color: 'white', fontSize: 30}}>
+                  {`device_name: ${printer.device_name}, inner_mac_address: ${printer.inner_mac_address}`}
+                </Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+          <View style={{flex: 1}}>
+            <Text style={{color: 'white', fontSize: 50}}>
+              List of USB devices
+            </Text>
+            {usbPrinters.map(printer => (
+              <TouchableOpacity
+                style={{
+                  backgroundColor: 'blue',
+                  borderRadius: 30,
+                  padding: 5,
+                  margin: 5,
+                }}
+                key={printer.device_id}
+                onPress={() => connectUsbPrinter(printer)}>
+                <Text style={{color: 'white', fontSize: 30}}>
+                  {`device_name: ${printer.device_name}, device_id: ${printer.device_id}, vendore_id: ${printer.vendor_id}, product_id: ${printer.product_id}`}
+                </Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            backgroundColor: 'black',
+            margin: 20,
+            padding: 10,
+            borderRadius: 20,
+          }}>
+          <TouchableOpacity
+            style={{
+              backgroundColor: 'red',
+              borderRadius: 30,
+              padding: 5,
+              margin: 5,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+            onPress={printBluetoothTextTest}>
+            <Text style={{color: 'white', fontSize: 30}}>Print Text</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              backgroundColor: 'red',
+              borderRadius: 30,
+              padding: 5,
+              margin: 5,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+            onPress={printBluetoothBillTest}>
+            <Text style={{color: 'white', fontSize: 30}}>Print Bill Text</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
